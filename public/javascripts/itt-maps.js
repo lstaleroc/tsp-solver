a92e128220d59e894f057db05b785b90d400842f
lstaleroc <luchotalcha@gmail.com>
2017-05-22 23:13:30 -0500

    Improvements in routes visualization

--- public/javascripts/itt-maps.js
+++ public/javascripts/itt-maps.js
@@ -9,7 +9,10 @@ function initMap() {
         center: {lat: 4.640081, lng:  -74.060613}
     });
     directionsDisplay.setMap(map);
-
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(4.640081,-74.060613)
+    });
+    marker.setMap(map);
     document.getElementById('submit').addEventListener('click', function() {
         calculateAndDisplayRoute(directionsService, directionsDisplay);
     });
@@ -40,14 +43,23 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
             var summaryPanel = document.getElementById('directions-panel');
             summaryPanel.innerHTML = '';
             // For each route, display summary information.
+            var totDist = 0;
+            var totDur = 0;
             for (var i = 0; i < route.legs.length; i++) {
                 var routeSegment = i + 1;
-                summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
+                summaryPanel.innerHTML += '<b>Ruta # ' + routeSegment +
                     '</b><br>';
-                summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+                summaryPanel.innerHTML += route.legs[i].start_address + ' <b> ---> </b> ';
                 summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
-                summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+                summaryPanel.innerHTML += '<b>Distancia: </b> '+ route.legs[i].distance.text + '<br>';
+                totDist += route.legs[i].distance.value;
+                summaryPanel.innerHTML += '<b>Tiempo aproximado: </b> '+ route.legs[i].duration.text + '<br><br>';
+                totDur += route.legs[i].duration.value;
             }
+            $("#tot-route-distance").empty();
+            $("#tot-route-duration").empty();
+            $("#tot-route-distance").append("<b>Distancia total ruta: </b> "+ (totDist/1000).toFixed(2).toString() + " kms");
+            $("#tot-route-duration").append("<b>Duración total ruta: </b> " + (totDur/60).toFixed(2).toString() + " mins");
         } else {
             window.alert('Directions request failed due to ' + status);
         }
